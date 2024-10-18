6bd1801e58f235d89d74d3904e9fd2fb62f85a8a
Thomas Scholtens <thomas.scholtens.scholtens@student.nhlstenden.com>
2024-10-18 10:52:27 +0200

    php include functie toegevoegd van de navbar voor alle pagina's

---
+++ functies/samenvoeg-nav.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+<header>
+        <nav>
+            <hr class="nav_line_top">
+            <div class="page_logo">
+                <a href="Frontpage.html">
+                    <img class="oui_koffie" src="../Img/illustrations_blue green_coffee.png" alt="coffie logo">
+                </a>
+                <a href="Frontpage.html">
+                    <img class="oui_logo" src="../Img/Logo-04.png" alt="oui_logo">
+                </a>
+            </div>
+            <ul class="link">
+                <li><a href="Frontpage.html">Home</a></li>
+                <li><a href="aboutus.html">About</a></li>
+                <li><a href="menu.html">Menu</a></li>
+                <li><a href="reviewPage.html">Reviews</a></li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+            <?php if (!empty($_SESSION['user'] )): ?>
+                <div class="logout_foto_container">
+                    <a href="../html/Frontpage.php"><img class="logout-logo" src="../img/logout-logo.svg" alt="logout-logo"></a>
+                </div>
+            <?php else:?>
+                <div class="Login_foto_container">
+                    <a href="../html/loginPage.php"><img class="login-logo" src="../img/login-logo.svg" alt="login-logo"></a>
+                </div>
+            <?php endif;?>
+            <hr class="nav_line_bottom">
+        </nav>
+    </header>
+</body>
+</html>
