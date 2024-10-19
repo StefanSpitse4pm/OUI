2d5b895ecdfb4f9c5ec81f0b402e8b9a7bf1f4dd
Luke Boscher <luke.boscher@student.nhlstenden.com>
2024-10-19 12:35:49 +0200

    loginfunctie fixed

--- functies/loginfunction.php
+++ functies/loginfunction.php
@@ -1,40 +1,36 @@
 <?php
-session_start();
-
-
-if (isset($_SESSION['username'])) {
-    header("Location: ../html/Frontpage.php");
-    exit();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
 }
-
 $users = [
     "user" => [ "email" => "user@user.com", "password" => "user"]
 ];
 
-
-
 $failed = '';
 
-if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
 
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
         $username = filter_input(INPUT_POST, "username");
         $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
         $password = filter_input(INPUT_POST, "password");
-    }
 
-    if (!empty($users[$username]) && $users[$username]['password'] === $password) {
-        if ($users[$username]['email'] === $email) {
-            $_SESSION['username'] = $email;
-            header("Location: ../html/Frontpage.php");
-            exit();
+        if (!empty($users[$username]) && $users[$username]['password'] === $password) {
+            if ($users[$username]['email'] === $email) {
+                $_SESSION['username'] = $username;
+                if (!empty($_SESSION)) {
+                    header("Location: ../html/Frontpage.php");
+                    exit();
+                }
+            } else {
+                $failed = "<div class='error'><h2>Ongeldige email of wachtwoord.</h2></div>";
+            }
         } else {
-            $failed = "<div class='error'><h2>Ongeldige email of wachtwoord.</h2></div>";
+            $failed = "<div class='error'><h2>Ongeldige gebruikersnaam of wachtwoord.</h2></div>";
         }
     } else {
-        $failed = "<div class='error'><h2>Ongeldige gebruikersnaam of wachtwoord.</h2></div>";
+        $failed = "<div class='error'><h2>Vul alle velden in.</h2></div>";
     }
-} else {
-    $failed = "<div class='error'><h2>Vul alle velden in.</h2></div>";
 }
 ?>
