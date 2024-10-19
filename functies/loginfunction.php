ffdc4427c3086898bd886e8623095f468e4b688c
Luke Boscher <luke.boscher@student.nhlstenden.com>
2024-10-19 13:14:52 +0200

    loginfunctie en pagina opgeschoond

--- functies/loginfunction.php
+++ functies/loginfunction.php
@@ -2,16 +2,13 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+
 $users = [
     "user" => [ "email" => "user@user.com", "password" => "user"]
 ];
 
-$failed = '';
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-
-    if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+function login($username, $email, $password, $users) {
+    if (!empty($username) && !empty($email) && !empty($password)) {
         $username = filter_input(INPUT_POST, "username");
         $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
         $password = filter_input(INPUT_POST, "password");
@@ -19,18 +16,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (!empty($users[$username]) && $users[$username]['password'] === $password) {
             if ($users[$username]['email'] === $email) {
                 $_SESSION['username'] = $username;
-                if (!empty($_SESSION)) {
-                    header("Location: ../html/Frontpage.php");
-                    exit();
-                }
+                header("Location: ../html/Frontpage.php");
+                exit();
             } else {
-                $failed = "<div class='error'><h2>Ongeldige email of wachtwoord.</h2></div>";
+                return "<div class='error'><h2>Ongeldige email of wachtwoord.</h2></div>";
             }
         } else {
-            $failed = "<div class='error'><h2>Ongeldige gebruikersnaam of wachtwoord.</h2></div>";
+            return "<div class='error'><h2>Ongeldige gebruikersnaam of wachtwoord.</h2></div>";
         }
     } else {
-        $failed = "<div class='error'><h2>Vul alle velden in.</h2></div>";
+        return "<div class='error'><h2>Vul alle velden in.</h2></div>";
     }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $failed = login($_POST['username'], $_POST['email'], $_POST['password'], $users);
 }
 ?>
