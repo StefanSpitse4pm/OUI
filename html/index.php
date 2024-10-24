324668fc57917a118b88bf8288313b4fed3deb27
Stefan Spitse <stefan@4property.com>
2024-10-24 13:40:15 +0200

    la carte changes

--- html/index.php
+++ html/index.php
@@ -1,9 +1,18 @@
+<?php 
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}   
+
+?>
+
 <!DOCTYPE html>
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cafe OUI</title>
+    <link rel="stylesheet" href="../css/reviewPage.css" type="text/css">
 </head>
 <body>
     <?php include "Frontpage.php" ?>
