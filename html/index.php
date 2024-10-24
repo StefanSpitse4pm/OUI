44b99174fa596b81a20f1b95d2a4ae9816e42790
Stefan Spitse <stefan@4property.com>
2024-10-24 13:14:18 +0200

    added index page

---
+++ html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cafe OUI</title>
+</head>
+<body>
+    <?php include "Frontpage.php" ?>
+    <?php include "Aboutus.html" ?>
+    <?php include "laCarte.html" ?>
+    <?php include "reviewPage.php" ?>
+    <?php include "footer.html" ?>
+</body>
+</html>
