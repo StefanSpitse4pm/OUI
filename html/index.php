5260544a787aba588388b51cecc2c6608495bdba
t1mk4p <tim.kap@student.nhlstenden.com>
2024-10-24 13:33:02 +0200

    link rel reviewPage.css toegevoegd in index.php

--- html/index.php
+++ html/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cafe OUI</title>
+    <link rel="stylesheet" href="../css/reviewPage.css" type="text/css">
 </head>
 <body>
     <?php include "Frontpage.php" ?>
