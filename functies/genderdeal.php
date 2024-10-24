9ce0802c2e1e2de5442cb7c9a7c584a04e0702e4
Luke Boscher <luke.boscher@student.nhlstenden.com>
2024-10-24 13:24:11 +0200

    genderdeal af en lacarte gefixt.

--- functies/genderdeal.php
+++ functies/genderdeal.php
@@ -1,16 +1,15 @@
 <?php
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
-
-
-    $users = [
-    "user" => [ "email" => "user@user.com", "password" => "user", "gender" => "man"]
-];
-    $gender = $users["user"]["gender"];
-    $_SESSION['gender'] = $gender;
 }
 
-require '../ProjectOuiGithub/functies/loginfunction.php'
+$users = [
+    "user" => [ "email" => "user@user.com", "password" => "user", "gender" => "vrouw"]
+];
+$gender = $users["user"]["gender"];
+$_SESSION['gender'] = $gender;
+
+// require '../functies/loginfunction.php'
 ?>
 
 <!DOCTYPE html>
@@ -19,16 +18,38 @@ require '../ProjectOuiGithub/functies/loginfunction.php'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/genderdeal.css">
 </head>
 <body>
-    <?php if ($_SESSION['gender'] == 'man'):?>
+    <?php if ($_SESSION['gender'] == 'man'): ?>
         <div class="genderdeal">
-
+            <img src="../Img/Logo-yellow.png" alt="logo" class="genderdeal-image">
+            <span class="genderdeal-text">Cafe OUI heeft Men's Night op 30 November!</span>
+            <img src="../Img/Logo-yellow.png" alt="logo" class="genderdeal-image">
+            <form method="post" style="display:inline;" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                <input type="submit" name="close_genderdeal" value="X" class="genderdeal-close">
+            </form>
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['close_genderdeal'])) {
+                echo '<style>.genderdeal { display: none; }</style>';
+            }
+            ?>
         </div>
-    <?php elseif ($_SESSION['gender'] == 'vrouw'):?>
-        <div class="genderdeal">
-
+    <?php elseif ($_SESSION['gender'] == 'vrouw'): ?>
+        <div class="genderdeal vrouw">
+            <img src="../Img/Logo-yellow.png" alt="logo" class="genderdeal-image">
+            <span class="genderdeal-text">Cafe OUI heeft Women Night op 30 December!</span>
+            <img src="../Img/Logo-yellow.png" alt="logo" class="genderdeal-image">
+            <form method="POST" style="display:inline;" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                <input type="submit" name="close_genderdeal" value="X" class="genderdeal-close">
+            </form>
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['close_genderdeal'])) {
+                $_SESSION['show_genderdeal'] = false;
+                echo '<style>.genderdeal { display: none; }</style>';
+            }
+            ?>
         </div>
-    <?php endif;?>
+    <?php endif; ?>
 </body>
 </html>
