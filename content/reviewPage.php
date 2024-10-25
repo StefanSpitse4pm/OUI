c1563bccd2001e7fcf23f9af2e3830c872c94e03
t1mk4p <tim.kap@student.nhlstenden.com>
2024-10-25 09:53:23 +0200

    debugging

--- content/reviewPage.php
+++ content/reviewPage.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
 <div class="reviewPage">
     <img src="img/frenchflag.png" alt="frankrijkVlag" class="frankrijkVlag" id="review">
-    <div class="main-review">
+    <div class="mainReview">
         <section class="reviewsSection">
             <div class="reviewCard">
                 <div class="reviewHeader">
